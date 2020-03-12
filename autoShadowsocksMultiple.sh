9cf977ca5fdc1211eb9cc9efc645e027dd17e3eb
App1990 <840172959@qq.com>
2020-03-12 10:56:33 +0800

    change port and update ss version

--- autoShadowsocksMultiple.sh
+++ autoShadowsocksMultiple.sh
@@ -5,17 +5,19 @@
 echo "Installing Shadowsocks..................................................."
 
 CONFIG_FILE=/etc/shadowsocks.json
-SS_SERVICE_FILE=/etc/systemd/system/ssserver.service
+#SS_SERVICE_FILE=/etc/systemd/system/ssserver.service
 SS_PASSWORD=qwer1234
-SS_PORT=8388
+#SS_PORT=8388
 #SS_IP=`ip route get 1|awk '{print $NF;exit}'`
 
 
 echo "root can install soft"
 #yum install -y python-setuptools && easy_install pip
-sudo yum -y install epel-release && sudo yum -y install python-pip
+#sudo yum -y install epel-release && sudo yum -y install python-pip
+sudo yum -y install libsodium && sudo yum -y install python3-pip
 
-pip install shadowsocks
+#pip install shadowsocks
+sudo pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip
 
 # creat shadowsocks config
 
@@ -23,20 +25,22 @@ cat << EOF | tee ${CONFIG_FILE}
 {
     "server":"0.0.0.0",
     "local_address": "127.0.0.1",
-    "local_port":1080,
+    "local_port":7080,
     "port_password":{
-          "8388":"qwer1234",
-          "8389":"qwer1234",
-          "8390":"qwer1234",
-          "8391":"qwer1234",
-          "8392":"qwer1234",
-          "8393":"qwer1234",
-          "8394":"qwer1234",
-          "8395":"qwer1234",
-          "8396":"qwer1234",
-          "8397":"qwer1234"
+          "7788":"qwer1234",
+          "7789":"qwer1234",
+          "7790":"qwer1234",
+          "7791":"qwer1234",
+          "7792":"qwer1234",
+          "7793":"qwer1234",
+          "7794":"qwer1234",
+          "7795":"qwer1234",
+          "7796":"qwer1234",
+          "7797":"qwer1234",
+          "7798":"qwer1234",
+          "7799":"qwer1234"
      },
-    "timeout":600,
+    "timeout":300,
     "method":"aes-256-gcm",
     "fast_open": false
 }
@@ -45,27 +49,30 @@ EOF
 
 # set shadowssocks.service start with system
 
-echo "create ${SS_SERVICE_FILE} && set ssserver.service start with system......"
+# echo "create ${SS_SERVICE_FILE} && set ssserver.service start with system......"
 
-cat << EOF | tee ${SS_SERVICE_FILE}
-[Unit]
-Description=ssserver
+# cat << EOF | tee ${SS_SERVICE_FILE}
+# [Unit]
+# Description=ssserver
 
-[Service]
-TimeoutStartSec=0
-ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
+# [Service]
+# TimeoutStartSec=0
+# ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
 
-[Install]
-WantedBy=multi-user.target
-EOF
+# [Install]
+# WantedBy=multi-user.target
+# EOF
 
-echo "start ssserver.service .................................................."
+ echo "start ssserver.service .................................................."
 
-systemctl start ssserver
-systemctl enable ssserver
+# systemctl start ssserver
+# systemctl enable ssserver
 
-echo "status ssserver.service ................................................."
+ssserver -c /etc/shadowsocks.json -d start
+#ssserver -c /etc/shadowsocks.json -d stop
 
-systemctl status ssserver -l
+# echo "status ssserver.service ................................................."
+
+# systemctl status ssserver -l
 
 echo "ALL Done................................................................."
